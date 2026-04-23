01880779af7d8441f045b7f9b0e25eba0dee99bb
Natas Pijoraitis <nataspijoraitis@gmail.com>
2026-04-23 13:26:28 +0200

    feat: connected database

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$db   = "alumni";
+$user = "administrator_user";
+$pass = "securepass";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+};
